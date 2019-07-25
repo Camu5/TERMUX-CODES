c47c4f2d836c1da176a9b11dd6a794f107cbcc6d
Krypt0N <53099952+Krypt0N-Geek@users.noreply.github.com>
2019-07-25 14:57:21 -0500

    Add files via upload

---
+++ MUDATOR/Test1.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#Coded by Krypt0N
+
+if [ -f MRIndie ] && [ -f MRock ] && [ -f MRPop]
+then 
+	clear 
+else 
+	echo -e "\e[1mPlease redownload Music Recomendator. There is some missing or damaged file" | lolcat 
+       	exit
+fi	
+
+toilet -f standard -F gay "Mudator"
+echo -e "\e[1mA Music Recomendator!" | lolcat -a 
+ echo -e "\e[1mCoded by Krypt0N" | lolcat
+  echo
+  echo -e "\e[91m------------------------------------------------------"
+ echo  -e "\e[93mPlease, choose a musical genre:" 
+PS3="#?" 
+ options=("Rock" "Indie/Alternative" "Pop" "Quit")
+ select opt in "${options[@]}"
+do
+    case $opt in
+        "Rock")
+            echo "Good Choice!" | lolcat -a
+	    bash MRock
+	    exit
+            ;;
+        "Indie/Alternative")
+            echo "Wondeful Choice!" | lolcat -a
+	    bash MRIndie
+	    exit 
+            ;;
+        "Pop")
+            echo "Likefuly Choice!" | lolcat -a
+	    bash MRPop
+	    exit
+            ;;
+        "Quit")
+	    echo "See ya later!" | lolcat -a 
+	    clear	
+            break
+	    ;;
+        *) echo "Invalid Choice... Try Again!";;
+    esac
+done
+
+if [ -f exitfile ];
+then 
+	rm exitfile 
+	exit
+fi 
